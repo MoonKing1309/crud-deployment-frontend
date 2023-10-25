3ec41b38ae2ecbad8571ea8e13d8f19da0ab37bf
Sasidev <conorken1@gmail.com>
2023-10-25 17:56:56 +0530

    Update editStudent.js

--- src/components/editStudent.js
+++ src/components/editStudent.js
@@ -38,7 +38,7 @@ function EditStudent()
     const handleSubmit = ()=>
     {
         const data = {name:newData[0],email:newData[1],rollNo:newData[2]}
-        Axios.put("http://localhost:4000/studentRoute/update-student/"+id,data)
+        Axios.put("https://crud-deployment-backend-2-cyo4.onrender.com/studentRoute/update-student/"+id,data)
         .then((res)=>{
             if(res.status===200)
                 alert("Document updated successfully")
